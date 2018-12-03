b652aae9bdb09febcd18f740ef43a8e90e9f016a
Carl Smith <carlfsmithiii@gmail.com>
2018-12-03 14:47:51 -0500

    created NavBar component

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+
+const NavBar = () => {
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" color="inherit">
+                        React & Material-UI Sample Application
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+export default NavBar;
